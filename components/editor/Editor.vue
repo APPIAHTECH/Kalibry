4cb2f8a4584926b4b4d94b46dd29f42eb69a4492
Stephen Appiah <eunisaesea@gmal.com>
2019-07-20 08:10:20 +0200

    ui-editor-dev

---
+++ components/editor/Editor.vue
@@ -0,0 +1,221 @@
+<template>
+<el-container>
+  <el-header>
+
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b">
+
+      <el-menu-item index="1">Kalibry</el-menu-item>
+
+      <el-submenu index="2">
+        <template slot="title">Archivo</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+
+      
+      <el-submenu index="3">
+        <template slot="title">Editar</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+
+
+      <el-submenu index="4">
+        <template slot="title">Ver</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+
+
+      <el-submenu index="5">
+        <template slot="title">Insertar</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+
+
+      <el-submenu index="6">
+        <template slot="title">Formato</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+
+
+      <el-submenu index="7">
+        <template slot="title">Estilos</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+
+      <el-submenu index="8">
+        <template slot="title">Tabla</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+
+
+      <el-submenu index="9">
+        <template slot="title">Formulario</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+
+
+      <el-submenu index="10">
+        <template slot="title">Herramientas</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+
+      
+      <el-submenu index="11">
+        <template slot="title">Ayuda</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      
+    </el-menu>
+     <!-- DOC ICONS-->
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1"><i class="el-icon-back"></i></el-menu-item>
+      <el-menu-item index="2"><i class="el-icon-right"></i></el-menu-item>
+      <el-menu-item index="3"><i class="el-icon-printer"></i></el-menu-item>
+      <el-menu-item index="4">
+          <el-select v-model="value" placeholder="Select">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+            :disabled="item.disabled">
+          </el-option>
+        </el-select>
+      </el-menu-item>
+
+      <el-menu-item index="5">
+        <el-select v-model="value" placeholder="Select">
+        <el-option-group
+          v-for="group in options"
+          :key="group.label"
+          :label="group.label">
+          <el-option
+            v-for="item in group.options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-option-group>
+      </el-select>
+      </el-menu-item>
+      <el-menu-item index="6"><i class="el-icon-bold"></i></el-menu-item>
+      <el-menu-item index="7"><i class="el-icon-edit"></i></el-menu-item>
+      <el-menu-item index="8"><i class="el-icon-edit"></i></el-menu-item>
+      <el-menu-item index="9"><i class="el-icon-edit"></i></el-menu-item>
+      <el-menu-item index="10"><i class="el-icon-edit"></i></el-menu-item>
+      <el-menu-item index="11"><i class="el-icon-edit"></i></el-menu-item>
+    </el-menu>
+  </el-header>
+
+  <el-row>
+    <el-col :span="12"><el-aside></el-aside></el-col>
+    <el-col><el-main></el-main></el-col>
+  </el-row>
+  
+</el-container>
+</template>
+
+<style scoped>
+  .el-container{
+    width: 100%;
+    position: relative;
+  }
+  .el-header{
+    width: 100%;
+    padding: 0;
+  }
+
+  .el-aside{
+    height: 100%;
+    position: absolute;
+  }
+  .el-main{
+    height: 600px;
+    padding: 0;
+  }
+  .el-menu-demo{
+    width: 100% !important;
+  }
+</style>
