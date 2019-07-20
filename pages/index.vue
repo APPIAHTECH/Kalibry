0cf4568cba4ef4ef8c347b200ac3cc5eb61fe87a
Stephen Appiah <stephenappiahtech@gmail.com>
2019-07-20 14:06:33 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,10 @@
 <template>
   <el-container>
     <Editor></Editor>
-
-    <el-footer></el-footer>
   </el-container>
 </template>
 
 <style>
-.el-footer{
-  background-color: #545c64;
-}
 </style>
 
 <script>
