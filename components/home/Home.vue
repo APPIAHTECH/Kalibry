61cf856dd6cb052a0845e99d4e762953e1227490
Stephen Appiah <eunisaesea@gmal.com>
2019-07-27 18:44:18 +0200

    ui-home added books & filter seacher and options

--- components/home/Home.vue
+++ components/home/Home.vue
@@ -55,6 +55,28 @@
             <el-menu-item index="14-3">Espiritual</el-menu-item>
         </el-submenu>
     </el-menu>
+
+    <el-main class="wrap">
+        <div class="kalibry-card-wrap" v-for="item in books" :key="item.value" :label="item.label" :value="item.value">
+            <div class="shoot">
+                <img :src="item.value" class="image">
+                <div class="shoot-option">
+                    <el-avatar class="nano" size="small" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"></el-avatar>
+                    <div class="shoot-suboption">
+                        <div class="text">
+                            <p class="sub-title">{{item.label}}</p>
+                        </div>
+                        <div class="btn-box">
+                            <el-popover placement="right" width="400" trigger="click">
+                                <p> Este libro ... </p>
+                                <el-button size="mini" slot="reference">Resumen</el-button>
+                            </el-popover>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </el-main>
 </div>
 </template>
 
@@ -62,6 +84,87 @@
 .home {
     width: 100%;
     height: auto;
+}
+
+.btn-box {
+    position: absolute;
+    left: 68%;
+    width: auto;
+    top: 8px;
+}
+
+.shoot-suboption {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+}
+
+.sub-title {
+    font-weight: 500;
+    font-size: 13px;
+    line-height: 1.3em;
+    color: #333;
+    margin: 0px 0 2px 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: 100%;
+    position: relative;
+    top: 5px;
+}
+
+.nano {
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+    position: relative;
+    top: 2px;
+}
+
+.kalibry-card-wrap {
+    position: relative;
+    clear: left;
+    background: #fff;
+    border-radius: 2px;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.07);
+    overflow: hidden;
+    width: 300px;
+    height: 420px;
+    margin: 10px;
+}
+
+.image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    background-position: center;
+    cursor: pointer;
+}
+
+.shoot {
+    padding: 10px;
+    transition: padding 0.2s ease;
+}
+
+.shoot-option {
+    box-sizing: border-box;
+    display: flex;
+    width: 100%;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    padding: 10px 10px 10px 10px;
+    background: #fff;
+    z-index: 2;
+    will-change: transition;
+    transition: height 0.2s ease;
+}
+
+.wrap {
+    background-color: #f4f4f4;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
 }
 
 .kalibry {
@@ -119,7 +222,41 @@ export default {
                 value: 'antiguo',
                 label: 'Antiguo'
             }],
-            value: 'Reciente'
+            value: 'Reciente',
+            books: [{
+                    value: "https://damonza.com/wp-content/uploads/portfolio/nonfiction/Love6.jpg",
+                    label: "Search for love"
+                },
+                {
+                    value: "https://damonza.com/wp-content/uploads/portfolio/nonfiction/Set%20For%20Life%202.jpg",
+                    label: "Set for life"
+                },
+                {
+                    value: "https://damonza.com/wp-content/uploads/portfolio/nonfiction/Journey%20of%20an%20Introvert%206.jpg",
+                    label: "Journy of an introvert"
+                },
+                {
+                    value: "https://damonza.com/wp-content/uploads/portfolio/nonfiction/amsterdam-exposed_05_A.jpg",
+                    label: "Amsterdam exposed"
+                },
+                {
+                    value: "https://damonza.com/wp-content/uploads/portfolio/nonfiction/voice1b.jpg",
+                    label: "Finding your voice"
+                },
+                {
+                    value: "https://damonza.com/wp-content/uploads/portfolio/nonfiction/whatnerve1.jpg",
+                    label: "What never"
+                },
+                {
+                    value: "https://damonza.com/wp-content/uploads/portfolio/nonfiction/Some-Are-Sicker-Than-OthersD5.jpg",
+                    label: "Some of them"
+                },
+                {
+                    value: "https://damonza.com/wp-content/uploads/portfolio/nonfiction/RTFM1b.jpg",
+                    label: "RTFM"
+                },
+
+            ]
         };
     },
     methods: {
